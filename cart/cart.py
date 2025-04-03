933aeecc9c44a0e700981dd1b53988a745a9129e
Chao <renchao.rachel@gmail.com>
2025-04-03 19:39:43 -0400

    Section 8: 44: total price functionality

--- cart/cart.py
+++ cart/cart.py
@@ -66,4 +66,8 @@ class Cart():
 
             yield item
 
-             
+
+    def get_total(self):
+
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+     
